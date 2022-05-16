08a3202213f602f8f613f25ac369c36ae1d05fdf
Vitaliy <miraigeweb@gmail.com>
2022-05-16 20:41:48 +0300

    logical operatorsЭ

--- udemi.js
+++ udemi.js
@@ -36,8 +36,26 @@
 
 
 //Интерпаляция
-let name = 'Vit';
-console.log(`Will be strong ${name}`);
+// let name = 'Vit';
+// console.log(`Will be strong ${name}`);
 
-let arrHobbys = ['sport', ' programming', ' swiming', ' musik'];
-console.log(`I like this hobbys ${arrHobbys} and i also study english.`);
+// let arrHobbys = ['sport', ' programming', ' swiming', ' musik'];
+// console.log(`I like this hobbys ${arrHobbys} and i also study english.`);
+
+//Логические операторы
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nugets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nugets) {//приоритет здесь отдается оператору сравнение, после оператор и, и потом уже идет сравнение или
+	console.log('Все довольны');
+} else {
+	console.log('Мы уходим');
+}
+
+if (hamburger === 3 && (cola === 2 || fries === 3) && nugets) {
+	console.log('!!!');//здесь приоритет вычислениябудет происходить изначально в скобках
+} else {
+	console.log(!0);
+}
