d9507c9db6bec1994b4c17f9e7ef0f8c3c656f2e
Vitaliy <miraigeweb@gmail.com>
2022-05-17 22:38:07 +0300

    twenty to ten

--- exercises.js
+++ exercises.js
@@ -1,6 +1,14 @@
 'use strict';
 
 //вывести числа от 5 до 10
-for (let i = 5; i <= 10; i++) {
-	console.log(i);
+// for (let i = 5; i <= 10; i++) {
+// 	console.log(i);
+// }
+
+//вывести числа в обатном порядке и остаовить если число равно 13
+for (let i = 20; i > 10; i--) {
+	if (i <= 13) {
+		console.log(i);
+		break;
+	}
 }
