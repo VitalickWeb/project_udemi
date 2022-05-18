76712d995bad52e8a846643d44a3fb55f1a9ca1c
Vitaliy <miraigeweb@gmail.com>
2022-05-18 22:14:24 +0300

    revers array, two options

--- exercises.js
+++ exercises.js
@@ -71,11 +71,17 @@
 // console.log(data);
 
 
+//revers array, two options
 const data = [5, 10, 'Shopping', 20, 'Homework'];
 const result = [];
 
 for (let i = data.length - 1; i >= 0; i--) {
-	console.log(i, data[i]);
 	result.push(data[i]);
 }
+
+for (let i = 1; i <= data.length; i++) {
+	result[i - 1] = data[data.length - i];
+}
 console.log(result);
+
+
