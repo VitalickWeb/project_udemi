d5b2b9866122afb9433a3fb8c8cf0e493fcd91fd
Vitaliy <miraigeweb@gmail.com>
2022-05-18 21:31:57 +0300

    array, number and string

--- exercises.js
+++ exercises.js
@@ -49,10 +49,23 @@
 
 
 //arrays
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
 
-for (let i = 0; i < arr.length; i++) {
-	result[i] = arr[i];
+// for (let i = 0; i < arr.length; i++) {
+// 	result[i] = arr[i];
+// }
+// console.log(result);
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+
+	if (typeof data[i] === 'string') {
+		data[i] += ' - done';
+	} else {
+		data[i] = +data[i] * 2;
+	}
 }
-console.log(result);
+console.log(data);
