16558012500505686ecefd2335154e2778ad6dc6
Vitaliy <miraigeweb@gmail.com>
2022-05-20 23:52:27 +0300

    practic cycle and if else

--- exercises.js
+++ exercises.js
@@ -87,17 +87,19 @@
 
 
 //figure building
-const lines = 5;
-let result = '';
+// const lines = 5;
+// let result = '';
 
-for (let i = 0; i <= lines; i++) {
-	for (let j = 0; j < lines - i; j++) {
-		result += " ";
-	}
-	for (let j = 0; j < 2 * i + 1; j++) {
-		result += "*";
-	}
-	result += "\n";
-}
+// for (let i = 0; i <= lines; i++) {
+// 	for (let j = 0; j < lines - i; j++) {
+// 		result += " ";
+// 	}
+// 	for (let j = 0; j < 2 * i + 1; j++) {
+// 		result += "*";
+// 	}
+// 	result += "\n";
+// }
 
-console.log(result)
+// console.log(result)
+
+
--- practic-udemi.js
+++ practic-udemi.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//практика часть 1
+//####################################################################
 // const numberOfFilms = +prompt('Сколькл фильмов вы уже посмотрели?', '');
 
 // let personalMoviesDB = {
@@ -19,3 +21,26 @@
 // personalMoviesDB['movies'][LastFilm2] = lastWotchOfFilms2;
 
 // console.log(personalMoviesDB);
+
+
+
+//Практика часть 2
+//########################################################################
+const numberOfFilms = +prompt('Сколькл фильмов вы уже посмотрели?', '');
+
+let personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+}
+
+for (let i = 0; i < 2; i++) {
+	const LastFilm1 = prompt('Один из последних просмотренных фильмов', '');
+	const lastWotchOfFilms1 = prompt('На сколько оцените его?', '');
+
+	personalMoviesDB['movies'][LastFilm1] = lastWotchOfFilms1;
+}
+
+console.log(personalMoviesDB);
